f7c7571b32ae28edd3ad2a39e284247f11c28a73
Atilist <71230342+Atilist@users.noreply.github.com>
2025-01-04 23:30:32 +0100

    Expanded Nether Portal Structure Rules
    Nether Portals now work like Vanilla in the Nether.
    Both Sides of the Entrance now need Active Engraved Obsidian Blocks on the Floor.

--- src/main/java/net/atilist/harderthanwolves/mixin/NetherPortalMixin.java
+++ src/main/java/net/atilist/harderthanwolves/mixin/NetherPortalMixin.java
@@ -3,6 +3,7 @@ package net.atilist.harderthanwolves.mixin;
 import net.atilist.harderthanwolves.events.init.BlockListener;
 import net.minecraft.block.NetherPortalBlock;
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.NetherDimension;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class NetherPortalMixin {
     @Inject(at = @At("HEAD"), method = "method_736", remap = false, cancellable = true)
     private void addIgnitionCondition(World world, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
-        boolean xPortal = (world.getBlockId(x + 1, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 2, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x + 1, y + 3, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 2, y + 3, z) == BlockListener.chiseledObsidian.id)
-                || (world.getBlockId(x + 2, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 1, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x + 2, y + 3, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 1, y + 3, z) == BlockListener.chiseledObsidian.id);
-        boolean zPortal = (world.getBlockId(x, y - 1, z + 1) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y - 1, z - 2) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z + 1) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z - 2) == BlockListener.chiseledObsidian.id)
-                || (world.getBlockId(x, y - 1, z + 2) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y - 1, z - 1) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z + 2) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z - 1) == BlockListener.chiseledObsidian.id);
+        if (world.dimension instanceof NetherDimension) {
+            return;
+        }
+        boolean xPortal = (world.getBlockId(x + 1, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 2, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x + 1, y + 3, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 2, y + 3, z) == BlockListener.chiseledObsidian.id
+                && world.getBlockId(x, y - 1, z + 1) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x - 1, y - 1, z + 1) == BlockListener.activeEngravedObsidian.id && world.getBlockId(x, y - 1, z - 1) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x - 1, y - 1, z - 1) == BlockListener.activeEngravedObsidian.id)
+                || (world.getBlockId(x + 2, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 1, y - 1, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x + 2, y + 3, z) == BlockListener.chiseledObsidian.id && world.getBlockId(x - 1, y + 3, z) == BlockListener.chiseledObsidian.id
+                && world.getBlockId(x + 1, y - 1, z + 1) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x, y - 1, z + 1) == BlockListener.activeEngravedObsidian.id && world.getBlockId(x + 1, y - 1, z - 1) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x, y - 1, z - 1) == BlockListener.activeEngravedObsidian.id);
+        boolean zPortal = (world.getBlockId(x, y - 1, z + 1) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y - 1, z - 2) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z + 1) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z - 2) == BlockListener.chiseledObsidian.id
+                && world.getBlockId(x + 1, y - 1, z) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x + 1, y - 1, z - 1) == BlockListener.activeEngravedObsidian.id && world.getBlockId(x - 1, y - 1, z) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x - 1, y - 1, z - 1) == BlockListener.activeEngravedObsidian.id)
+                || (world.getBlockId(x, y - 1, z + 2) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y - 1, z - 1) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z + 2) == BlockListener.chiseledObsidian.id && world.getBlockId(x, y + 3, z - 1) == BlockListener.chiseledObsidian.id
+                && world.getBlockId(x + 1, y - 1, z + 1) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x + 1, y - 1, z) == BlockListener.activeEngravedObsidian.id && world.getBlockId(x - 1, y - 1, z + 1) == BlockListener.activeEngravedObsidian.id  && world.getBlockId(x - 1, y - 1, z) == BlockListener.activeEngravedObsidian.id);
         if (!xPortal && !zPortal) {
             cir.setReturnValue(false);
         }
